d73b8f4032f534ffb82018f0549fbba870b07ba6
cornel.siclovan <cornel.siclovan@gmail.com>
2018-06-22 16:13:59 +0300

    1234

--- src/Controller/PlacesController.php
+++ src/Controller/PlacesController.php
@@ -9,6 +9,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Place;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,20 +18,48 @@ use Symfony\Component\HttpFoundation\Response;
 class PlacesController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(EntityManagerInterface $em)
     {
-        return new Response('places are here');
+        $repository = $em->getRepository(Place::class);
+        /** @var Place[] $places */
+        $places = $repository->findAll();
+
+        return $this->render('places/homepage.html.twig',[
+            'places' => $places
+        ]);
+
     }
 
     /**
-     * @Route("/places/{slug}")
+     * @Route("/places/{slug}", name="place_show")
      */
     public function show($slug)
     {
+
         return $this->render('places/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug))
         ]);
+    }
+
+    /**
+     * @Route("admin/place/new", name="new_place")
+     */
+    public function new(EntityManagerInterface $em)
+    {
+        $place = new Place();
+        $place->setName("locatie 1")
+            ->setSlug("locatie 1")
+            ->setAddress("adresa locatie 1")
+            ->setCity("Timisoara")
+            ->setEmail("loc1@gmail.com")
+            ->setTelNumber("078888999")
+            ->setCountry("Romania")
+            ->setWebsite("www.loc1.com");
+
+        $em->persist($place);
+        $em->flush();
+        return new Response('place added');
     }
 }
---
+++ src/Entity/Place.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PlaceRepository")
+ */
+class Place
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tel_number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $website;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelNumber(): ?string
+    {
+        return $this->tel_number;
+    }
+
+    public function setTelNumber(string $tel_number): self
+    {
+        $this->tel_number = $tel_number;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
